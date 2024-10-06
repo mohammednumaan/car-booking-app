7cdf1beb8a0ae08c12e6b27dfb8171be44a63668
Risheekesh K G <rishikesh9894318193@gmail.com>
2024-10-06 14:42:22 +0530

    Added Account Page, Made Document page and account pageresponsive

--- front-end/src/components/Account/Account.jsx
+++ front-end/src/components/Account/Account.jsx
@@ -1,8 +1,27 @@
-export default function Account(){
-    return (
+import Avatar from '@mui/material/Avatar';
+import { blue } from '@mui/material/colors';
+import style from "./Account.module.css";
 
-        <div>
-            <h1>Account</h1>
+export default function Account() {
+    const profileData = {
+        name: "Risheekesh",
+        email: "Risheekesh@example.com",
+        phone: "123456789",
+        ID : "223344",
+    };
+
+    return (
+        <div className={style['root-card']}>
+            <div className={style['profile-card']}>
+                <Avatar sx={{ bgcolor: blue[500], width: "150px", height: "150px" }}>OP</Avatar>
+            </div>
+            <div className={style['content-card']}>
+                <h2>{profileData.name}</h2>
+                <p>Staff ID : {profileData.ID}</p>
+                <p>Email: {profileData.email}</p>
+                <p>Phone: {profileData.phone}</p>
+                
+            </div>
         </div>
     );
 }
